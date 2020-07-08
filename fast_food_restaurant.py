b1411c38acfd7d800f593cca7ea6520da58c78c5
domyselfzhen123 <993566949@qq.com>
2020-07-08 21:10:05 +0800

    提交对于数据输入的优化及MongoDB解决思路

--- fast_food_restaurant.py
+++ fast_food_restaurant.py
@@ -13,15 +13,12 @@ from abc import ABC, abstractmethod
 class Promotion(ABC):
 
     def __init__(self):
-        self.food_price_map = {
-            "ITEM0001":18,
-            "ITEM0013":6,
-            "ITEM0022":8,
-        }
-        self.satisfy_money = 30
-        self.reduce_money = 6
-        self.discount_food = ["ITEM0001","ITEM0022"]
-        self.discount_number = 0.5
+        u = Utils()
+        self.food_price_map = u.initdata_to_food_price_map()
+        self.satisfy_money = InitData['config']['satisfy_money']
+        self.reduce_money = InitData['config']['reduce_money']
+        self.discount_food = InitData['config']['discount_food']
+        self.discount_number =InitData['config']['discount_number']
 
     @abstractmethod
     def discount(self, order_dict):
@@ -74,17 +71,12 @@ class Order(object):
 class Utils(object):
 
     def __init__(self):
-        self.split_sign = "x"
-        self.food_price_map = {
-            "ITEM0001":18,
-            "ITEM0013":6,
-            "ITEM0022":8,
-        }
-        self.food_name_map = {
-            "ITEM0001":"黄焖鸡",
-            "ITEM0013":"肉夹馍",
-            "ITEM0022":"凉皮",
-        }
+        self.foods = 'foods'
+        self.split_sign = InitData['config']['sign']
+        self.satisfy_money = InitData['config']['satisfy_money']
+        self.reduce_money = InitData['config']['reduce_money']
+        self.food_price_map = self.initdata_to_food_price_map()
+        self.food_name_map = self.initdata_to_food_name_map()
         self.discount_food_list = ["ITEM0001","ITEM0022"]
 
     def list_split_dict(self,datalist):
@@ -113,12 +105,13 @@ class Utils(object):
         print("使用优惠:")
         if model == "Reduce":
             #满30减6元，省6元
-            print("满30减6元，省6元")
+            print("满{satisfy_money}减{reduce_money}元，省{reduce_money}元".format(satisfy_money=self.satisfy_money,
+                                                                             reduce_money=self.reduce_money))
         elif model == "Discount":
             #指定菜品半价(黄焖鸡，凉皮)，省13元
             foods = ",".join([self.food_name_map[_] for _ in foodlist if _ in self.discount_food_list])
             reduce_money = original_price-nodeprice
-            print("指定菜品半价({foods})，省{reduce_money}元".format(foods=foods,reduce_money=reduce_money))
+            print("指定菜品半价({foods})，省{reduce_money}元".format(foods=foods,reduce_money=int(reduce_money)))
         print("-----------------------------------")
 
     def total_printing(self,nodeprice):
@@ -126,29 +119,61 @@ class Utils(object):
         print("总计：{nodeprice}元".format(nodeprice=int(nodeprice)))
         print("===================================")
 
+    def initdata_to_food_price_map(self):
+        foodslist = InitData[self.foods]
+        food_price_map = dict()
+        for food_details in foodslist:
+            food_price_map[food_details['id']] = food_details['price']
+        return food_price_map
+
+    def initdata_to_food_name_map(self):
+        foodslist = InitData[self.foods]
+        food_name_map = dict()
+        for food_details in foodslist:
+            food_name_map[food_details['id']] = food_details['name']
+        return food_name_map
+
+    def initdata_to_discount_food_list(self):
+        foodslist = InitData[self.foods]
+        discount_food_list = list()
+        for food_details in foodslist:
+            if food_details['is_half_price']:
+                discount_food_list.append(food_details['id'])
+        return discount_food_list
+
 
 DiscountCategory = ["Reduce","Discount"]
 Original = "Original"
-# InitData = {
-#     'ITEM0001': {
-#         'id': 'ITEM0001',
-#         'name': '黄焖鸡',
-#         'price': 18.0,
-#         'is_half_price': True  # 是否半价
-#     },
-#     'ITEM0013': {
-#         'id': 'ITEM0013',
-#         'name': '肉夹馍',
-#         'price': 6.0,
-#         'is_half_price': False  # 是否半价
-#     },
-#     'ITEM0022': {
-#         'id': 'ITEM0022',
-#         'name': '凉皮',
-#         'price': 8.0,
-#         'is_half_price': True  # 是否半价
-#     }
-# }
+InitData = {
+    'config':{
+        'sign':'x',
+        'satisfy_money':30,
+        'reduce_money':6,
+        'discount_food':["ITEM0001","ITEM0022"],
+        'discount_number':0.5
+    },
+    'foods':[
+        {
+            'id':'ITEM0001',
+            'name': '黄焖鸡',
+            'price': 18,
+            'is_half_price': True  # 是否半价
+        },
+        {
+            'id':'ITEM0013',
+            'name': '肉夹馍',
+            'price': 6,
+            'is_half_price': False  # 是否半价
+        },
+        {
+            'id':'ITEM0022',
+            'name': '凉皮',
+            'price': 8,
+            'is_half_price': True  # 是否半价
+        }
+    ]
+
+}
 
 def bestCharge(order_list):
     u = Utils()
@@ -171,7 +196,7 @@ def bestCharge(order_list):
 
 
 if __name__ == '__main__':
-    # order_list = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"]
+    order_list = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"]
     # order_list = ["ITEM0013 x 4", "ITEM0022 x 1"]
-    order_list = ["ITEM0013 x 4"]
+    # order_list = ["ITEM0013 x 4"]
     bestCharge(order_list)
